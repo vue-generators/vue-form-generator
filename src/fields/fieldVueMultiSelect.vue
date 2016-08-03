24c49d9254498fea8c23e9408f1adcb4a912e87f
Lionel Bijaoui <lionel.bijaoui@aviitam.com>
2016-08-03 14:33:02 +0200

    fix: lint error and warning

--- src/fields/fieldVueMultiSelect.vue
+++ src/fields/fieldVueMultiSelect.vue
@@ -30,57 +30,57 @@
 		)
 </template>
 <script>
-import { isObject } from "lodash";
+// import { isObject } from "lodash";
 import abstractField from "./abstractField";
-import Multiselect from 'vue-multiselect';
+import Multiselect from "vue-multiselect";
 
 export default {
-    mixins: [abstractField],
-    components: {
-        Multiselect
-    },
-    computed: {
-        options() {
-            let values = this.schema.values;
-            if (typeof(values) == "function") {
-                return values.apply(this, [this.model, this.schema]);
-            } else {
-                return values;
-            }
-        }
-    },
+	mixins: [abstractField],
+	components: {
+		Multiselect
+	},
+	computed: {
+		options() {
+			let values = this.schema.values;
+			if (typeof(values) == "function") {
+				return values.apply(this, [this.model, this.schema]);
+			} else {
+				return values;
+			}
+		}
+	},
 
-    methods: {
-        updateSelected(value, id) {
-            this.value = value;
-        },
-        onSelect(selectedOption, id) {
-            console.log("onSelect", selectedOption, id)
-        },
-        onRemove(removedOption, id) {
-            console.log("onRemove", removedOption, id)
-        },
-        onSearchChange(searchQuery, id) {
-            console.log("onSearchChange", searchQuery, id)
-        },
-        addTag(newTag, id) {
-            console.log("addTag", newTag, id);
-            // TODO: implement tag object by sending this function into schema definition
+	methods: {
+		updateSelected(value, id) {
+			this.value = value;
+		},
+		onSelect(selectedOption, id) {
+			console.log("onSelect", selectedOption, id);
+		},
+		onRemove(removedOption, id) {
+			console.log("onRemove", removedOption, id);
+		},
+		onSearchChange(searchQuery, id) {
+			console.log("onSearchChange", searchQuery, id);
+		},
+		addTag(newTag, id) {
+			console.log("addTag", newTag, id);
+			// TODO: implement tag object by sending this function into schema definition
 			/* const tag = {
                 name: newTag,
                 // Just for example needs as we use Array of Objects that should have other properties filled.
                 // For primitive values you can simply push the tag into options and selected arrays.
                 code: newTag.substring(0, 2) + Math.floor((Math.random() * 10000000))
             }*/
-            this.options.push(newTag)
-            this.value.push(newTag)
-        },
-        onOpen(id) {
-            console.log("onOpen", id)
-        },
-        onClose(value, id) {
-            console.log("onClose", value, id)
-        },
-    }
+			this.options.push(newTag);
+			this.value.push(newTag);
+		},
+		onOpen(id) {
+			console.log("onOpen", id);
+		},
+		onClose(value, id) {
+			console.log("onClose", value, id);
+		}
+	}
 };
 </script>
