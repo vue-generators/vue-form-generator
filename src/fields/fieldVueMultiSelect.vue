dba3ed710f17485ba634bda5cc06de62b8aa2d57
Lionel Bijaoui <lionel.bijaoui@aviitam.com>
2016-08-05 13:23:23 +0200

    Add all remainning props and some events (`onNewTag` & `onSearch`) from multiselect and update dev schema.

--- src/fields/fieldVueMultiSelect.vue
+++ src/fields/fieldVueMultiSelect.vue
@@ -1,83 +1,85 @@
 <template lang="jade">
-	multiselect(
-		:id="schema.selectOptions.id", 
+	multiselect(		
+		:id="schema.selectOptions.id",
 		:options="options",
-		:multiple="schema.multiSelect", 
-		:selected="value", 
-		:key="schema.selectOptions.key || null", 
-		:label="schema.selectOptions.label || null", 
-		:searchable="schema.selectOptions.searchable", 
-		:clear-on-select="schema.selectOptions.clearOnSelect", 
-		:hide-selected="schema.selectOptions.hideSelected", 
-		:placeholder="schema.placeholder", 
-		:max-height="schema.selectOptions.maxHeight", 
-		:allow-empty="schema.selectOptions.allowEmpty", 
-		:reset-after="schema.selectOptions.resetAfter", 
-		:close-on-select="schema.selectOptions.closeOnSelect", 
+		:multiple="schema.multiSelect",
+		:selected="value",
+		:key="schema.selectOptions.key || null",
+		:label="schema.selectOptions.label || null",
+		:searchable="schema.selectOptions.searchable",
+		:clear-on-select="schema.selectOptions.clearOnSelect",
+		:hide-selected="schema.selectOptions.hideSelected",
+		:placeholder="schema.placeholder",
+		:max-height="schema.selectOptions.maxHeight",
+		:allow-empty="schema.selectOptions.allowEmpty",
+		:reset-after="schema.selectOptions.resetAfter",
+		:close-on-select="schema.selectOptions.closeOnSelect",
 		:custom-label="schema.selectOptions.customLabel || null",
 		:taggable="schema.selectOptions.taggable",
 		:tag-placeholder="schema.selectOptions.tagPlaceholder",
-		:max="schema.selectOptions.max",
-		@update="updateSelected", 
-		@select="onSelect", 
-		@remove="onRemove", 
-		@search-change="onSearchChange", 
-		@tag="addTag", 
-		@open="onOpen", 
-		@close="onClose", 
-		:show-labels="schema.selectOptions.showLabels"
-		// TODO: add other options from multiSelectMixin
-		)
+		:max="schema.max",
+		@update="updateSelected",
+		@tag="addTag",
+		@select="onSelect",
+		@remove="onRemove",
+		@search-change="onSearchChange",
+		@open="onOpen",
+		@close="onClose",
+		:show-pointer="schema.selectOptions.showPointer",
+		:select-label="schema.selectOptions.selectLabel",
+		:selected-label="schema.selectOptions.selectedLabel",
+		:deselect-label="schema.selectOptions.deselectLabel",
+		:show-labels="schema.selectOptions.showLabels",
+		:limit="schema.selectOptions.limit",
+		:limit-text="schema.selectOptions.limitText",
+		:loading="schema.selectOptions.loading",
+		:disabled="schema.disabled",
+	)
 </template>
 <script>
-// import { isObject } from "lodash";
 import abstractField from "./abstractField";
 
 export default {
-	mixins: [abstractField],
-	computed: {
-		options() {
-			let values = this.schema.values;
-			if (typeof(values) == "function") {
-				return values.apply(this, [this.model, this.schema]);
-			} else {
-				return values;
-			}
-		}
-	},
-
-	methods: {
-		updateSelected(value, id) {
-			this.value = value;
-		},
-		onSelect(selectedOption, id) {
-			console.log("onSelect", selectedOption, id);
-		},
-		onRemove(removedOption, id) {
-			console.log("onRemove", removedOption, id);
-		},
-		onSearchChange(searchQuery, id) {
-			console.log("onSearchChange", searchQuery, id);
-		},
-		addTag(newTag, id) {
-			console.log("addTag", newTag, id);
-			// TODO: implement tag object by sending this function into schema definition
-			/* const tag = {
-                name: newTag,
-                // Just for example needs as we use Array of Objects that should have other properties filled.
-                // For primitive values you can simply push the tag into options and selected arrays.
-                code: newTag.substring(0, 2) + Math.floor((Math.random() * 10000000))
-            }*/
-			this.options.push(newTag);
-			this.value.push(newTag);
-		},
-		onOpen(id) {
-			console.log("onOpen", id);
-		},
-		onClose(value, id) {
-			console.log("onClose", value, id);
-		}
-	}
+    mixins: [abstractField],
+    computed: {
+        options() {
+            let values = this.schema.values;
+            if (typeof(values) == "function") {
+                return values.apply(this, [this.model, this.schema]);
+            } else {
+                return values;
+            }
+        }
+    },
+    methods: {
+        updateSelected(value, id) {
+            this.value = value;
+        },
+        addTag(newTag, id) {
+            let onNewTag = this.schema.selectOptions.onNewTag;
+            if (typeof(onNewTag) == "function") {
+                onNewTag(newTag, id, this.options, this.value);
+            }
+        },
+        onSearchChange(searchQuery, id) {
+            let onSearch = this.schema.selectOptions.onSearch;
+            if (typeof(onSearch) == "function") {
+                onSearch(searchQuery, id, this.options);
+            }
+        },
+        onSelect(selectedOption, id) {
+            // console.log("onSelect", selectedOption, id);
+        },
+        onRemove(removedOption, id) {
+            // console.log("onRemove", removedOption, id);
+        },
+        onOpen(id) {
+            // console.log("onOpen", id);
+        },
+        onClose(value, id) {
+            // console.log("onClose", value, id);
+        }
+    },
     created() {
         // Check if the component is loaded
         if (window.VueMultiselect) {
