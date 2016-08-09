49d122ee3f4a36169ca8a0dd136c6c6a06db8fdc
Icebob <mereg.norbert@gmail.com>
2016-08-09 14:46:55 +0200

    styles: fix .wrapper styles

--- src/fields/fieldImage.vue
+++ src/fields/fieldImage.vue
@@ -67,38 +67,41 @@
 </script>
 
 <style lang="sass">
-.vue-form-generator .field-image {
-	&.wrapper {
-		width: 100%;
-	}
-	.preview {
-		position: relative;
-		margin-top: 5px;
-		height: 100px;
-		background-repeat: no-repeat;
-		background-size: contain;
-		background-position: center center;						
-		border: 1px solid #ccc;
-		border-radius: 3px;
-		box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
 
-		.remove {
-			/*background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAXUlEQVR42u2SwQoAIAhD88vVLy8KBlaS0i1oJwP3piGVg0Skmpq8HjqZrWl9uwCbGAmwKYGZs/6iqgMyAdJuM8W2QmYKpLt/0AG9ASCv/oAnANd3AEjmAlFT1BypAV+PnRH5YehvAAAAAElFTkSuQmCC');
-			width: 16px;
-			height: 16px;
-			*/
-			font-size: 1.2em;
+	.vue-form-generator .field-image {
+		
+		.wrapper {
+			width: 100%;
+		}
 
-			position: absolute;
-			right: 0.2em;
-			bottom: 0.2em;
-			opacity: 0.7;
+		.preview {
+			position: relative;
+			margin-top: 5px;
+			height: 100px;
+			background-repeat: no-repeat;
+			background-size: contain;
+			background-position: center center;						
+			border: 1px solid #ccc;
+			border-radius: 3px;
+			box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
 
-			&:hover {
-				opacity: 1.0;
-				cursor: pointer;
+			.remove {
+				/*background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAXUlEQVR42u2SwQoAIAhD88vVLy8KBlaS0i1oJwP3piGVg0Skmpq8HjqZrWl9uwCbGAmwKYGZs/6iqgMyAdJuM8W2QmYKpLt/0AG9ASCv/oAnANd3AEjmAlFT1BypAV+PnRH5YehvAAAAAElFTkSuQmCC');
+				width: 16px;
+				height: 16px;
+				*/
+				font-size: 1.2em;
+
+				position: absolute;
+				right: 0.2em;
+				bottom: 0.2em;
+				opacity: 0.7;
+
+				&:hover {
+					opacity: 1.0;
+					cursor: pointer;
+				}
 			}
 		}
 	}
-}
 </style>
