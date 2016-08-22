776f6c479fec8105df55d69288a310cf58b2386c
Icebob <mereg.norbert@gmail.com>
2016-08-22 16:27:04 +0200

    test: add external libs to karma

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -11,7 +11,23 @@ module.exports = function(config) {
 
         frameworks: ['mocha', 'chai', 'sinon-chai'],
 
-        files: ['./index.js'],
+        files: [
+            'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/js/bootstrap-select.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.1.4/js/ion.rangeSlider.js',
+            'https://rawgit.com/monterail/vue-multiselect/master/lib/vue-multiselect.min.js',
+            'https://rawgit.com/nosir/cleave.js/master/dist/cleave.min.js',
+            'https://nosir.github.io/cleave.js/lib/cleave-phone.i18n.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/8.5.1/nouislider.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.4.0/pikaday.min.js',
+
+            './index.js'
+        ],
 
         exclude: [],
 
