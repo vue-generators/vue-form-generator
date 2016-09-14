5189b9962892df9fb2a2fb2b98daa3e12fdb7403
Lionel Bijaoui <lionel.bijaoui@aviitam.com>
2016-09-14 17:59:28 +0200

    new: new field html5 input

---
+++ src/fields/fieldHtml5.vue
@@ -0,0 +1,64 @@
+<template lang="jade">
+.wrapper
+	input.form-control(
+		:type="schema.inputType", 
+		v-model="value",
+		:disabled="disabled",
+
+		:accept="schema.accept",
+		:alt="schema.alt",
+		:autocomplete="schema.autocomplete",
+		:checked="schema.checked",
+		:dirname="schema.dirname",
+		:formaction="schema.formaction",
+		:formenctype="schema.formenctype",
+		:formmethod="schema.formmethod",
+		:formnovalidate="schema.formnovalidate",
+		:formtarget="schema.formtarget",
+		:height="schema.height",
+		:list="schema.list",
+		:max="schema.max",
+		:maxlength="schema.maxlength",
+		:min="schema.min",
+		:multiple="schema.multiple",
+		:pattern="schema.pattern",
+		:placeholder="schema.placeholder",
+		:readonly="schema.readonly",
+		:required="schema.required",
+		:size="schema.size",
+		:src="schema.src",
+		:step="schema.step",
+		:width="schema.width",
+		:files="schema.files")
+	span.helper(v-if="schema.inputType === 'color' || schema.inputType === 'range'") {{ value }}
+</template>
+
+<script>
+	import abstractField from "./abstractField";
+
+	export default {
+		mixins: [ abstractField ]
+	};
+	
+</script>
+
+<style lang="sass">
+	.vue-form-generator .field-html5 {
+		.wrapper {
+			width: 100%;
+		}
+		input[type="radio"] {
+			width: 100%;
+		}
+		input[type="color"] {
+			width: 60px;
+		}
+		input[type="range"] {
+			padding: 0;
+		}
+
+		.helper {
+			margin: auto 0.5em;
+		}
+	}
+</style>
