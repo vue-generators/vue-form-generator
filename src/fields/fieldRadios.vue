7106394f9d2140402c121377d21ddd5cce6036c2
Lionel Bijaoui <lionel.bijaoui@aviitam.com>
2016-09-27 11:34:51 +0200

    new: new "radios" field

---
+++ src/fields/fieldRadios.vue
@@ -0,0 +1,69 @@
+<template lang="jade">
+	.radio-list(:disabled="disabled")
+		label(v-for="item in items")
+			input(type="radio", :disabled="disabled", :name="id", @click="onSelection(item)", :value="getItemValue(item)")
+			| {{ getItemName(item) }}
+
+</template>
+
+<script>
+	import {isObject} from "lodash";
+	import abstractField from "./abstractField";
+	
+	export default {
+		mixins: [ abstractField ],
+
+		computed: {
+			items() {
+				let values = this.schema.values;
+				if (typeof(values) == "function") {
+					return values.apply(this, [this.model, this.schema]);
+				} else {
+					return values;
+				}
+			},
+			id(){
+				return this.schema.model;
+			}
+		},
+
+		methods: {
+			onSelection(item) {
+				if (isObject(item) && this.schema.radiosOptions.value && item[this.schema.radiosOptions.value]){
+					this.value = item[this.schema.radiosOptions.value];
+				} else{
+					this.value = item;
+				}
+			},
+			getItemValue(item) {
+				if (isObject(item) && this.schema.radiosOptions.value && item[this.schema.radiosOptions.value]){
+					return item[this.schema.radiosOptions.value];
+				}
+
+				return item;
+			},
+			getItemName(item) {
+				if (isObject(item) && this.schema.radiosOptions.name && item[this.schema.radiosOptions.name]){
+					return item[this.schema.radiosOptions.name];
+				}
+
+				return item;
+			}
+		}
+	};
+</script>
+
+<style lang="sass">
+	.vue-form-generator .field-radios {
+
+		.radio-list {
+			label {
+				display: block;
+				input[type="radio"]{
+					margin-right: 5px;
+				}
+			}
+		}
+
+	}
+</style>
