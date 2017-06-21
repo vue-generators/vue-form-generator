b4d2b34be1665cb647088f75ef8cf7b907259519
Lionel Bijaoui <lionel.bijaoui@aviitam.com>
2017-06-21 12:04:48 +0200

    Add an option to hide the none selected text

--- src/fields/core/fieldSelect.vue
+++ src/fields/core/fieldSelect.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 	select.form-control(v-model="value", :disabled="disabled", :name="schema.inputName", :id="getFieldID(schema)")
-		option(:disabled="schema.required", :value="null", :selected="value == undefined") {{ selectOptions.noneSelectedText || "&lt;Nothing selected&gt;" }}
+		option(v-if="!selectOptions.hideNoneSelectedText", :disabled="schema.required", :value="null", :selected="value == undefined") {{ selectOptions.noneSelectedText || "&lt;Nothing selected&gt;" }}
 		option(v-for="item in items", :value="getItemID(item)") {{ getItemName(item) }}
 </template>
 
