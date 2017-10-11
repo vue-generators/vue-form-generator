5b74eadbb41459509bb2f25bf9172da64bbc0bc3
hansi90 <grob.hans90@gmail.com>
2017-10-11 15:02:11 +0200

    New field: file upload (fieldFile).

---
+++ src/fields/core/fieldFile.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+.wrapper
+	input.form-control(
+		id="ts01",
+		type="file",
+		:name="schema.inputName",
+		@change="schema.onChange || null",	
+		:accept="schema.accept",
+		:multiple="schema.multiple",
+		:placeholder="schema.placeholder",
+		:readonly="schema.readonly",
+		:required="schema.required",
+		:disabled="disabled",)
+</template>
+
+<script>
+import abstractField from "../abstractField";
+import fecha from "fecha";
+
+export default {
+	mixins: [abstractField]
+};
+
+</script>
+
+<style lang="sass">
+	.vue-form-generator .field-input {
+		.wrapper {
+			width: 100%;
+		}
+		input[type="radio"] {
+			width: 100%;
+		}
+		input[type="color"] {
+			width: 60px;
+		}
+		input[type="range"] {
+			padding: 0;
+		}
+
+		.helper {
+			margin: auto 0.5em;
+		}
+	}
+</style>
