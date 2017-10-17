d74f7fd0e9c175b7d01f7bee0610f3822ed8e300
Luke Ramsden <hello@lukeramsden.com>
2017-10-17 13:32:05 +0100

    Added fieldDynamic scaffold

---
+++ src/fields/core/fieldDynamic.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .wrapper
+</template>
+
+<script>
+import abstractField from "../abstractField";
+
+export default {
+  mixins: [abstractField]
+};
+
+</script>
+
+<style lang="sass">
+</style>
