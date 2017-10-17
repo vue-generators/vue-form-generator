c0001be9fda50c81a558ce350ef7607e7f3cd5ab
Luke Ramsden <hello@lukeramsden.com>
2017-10-17 14:33:29 +0100

    Added delete button

--- src/fields/core/fieldDynamic.vue
+++ src/fields/core/fieldDynamic.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="wrapper">
-    <div v-for="(row, index) in value">
-      <vue-form-generator :schema="schema.schema" :model="value[index]" :options="formOptions"></vue-form-generator>
-    </div>
-  </div>
+	<div class="wrapper">
+		<button @click="add" class="add">Add</button>
+		<div v-for="(row, index) in value">
+			<vue-form-generator :schema="schema.schema" :model="value[index]" :options="formOptions"></vue-form-generator>
+			<button @click="remove(index)">Delete</button>
+		</div>
+	</div>
 </template>
 
 <script>
 import abstractField from "../abstractField";
 
 export default {
-  mixins: [abstractField]
+	mixins: [abstractField],
+	methods: {
+		add() {
+			this.value.push({});
+		},
+		remove(i) {
+			this.value.splice(i, 1);
+		}
+	}
 };
 
 </script>
 
 <style lang="sass">
+.add {
+	float: right;
+}
 </style>
