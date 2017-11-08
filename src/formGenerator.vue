8b6801b515f0f49c705775e2fb224698e420bc7f
David Higgins <dhiggins@aimmedia.com>
2017-11-08 13:55:14 -0500

    added styleClasses support to groups

--- src/formGenerator.vue
+++ src/formGenerator.vue
@@ -17,7 +17,7 @@ div.vue-form-generator(v-if='schema != null')
 					span(v-for='(error, index) in fieldErrors(field)', track-by='index') {{ error }}
 
 	template(v-for='group in groups')
-		fieldset(:is='tag')
+		fieldset(:is='tag', :class='getFieldRowClasses(group)')
 			legend(v-if='group.legend') {{ group.legend }}
 			template(v-for='field in group.fields')
 				.form-group(v-if='fieldVisible(field)', :class='getFieldRowClasses(field)')
