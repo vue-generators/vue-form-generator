3208446234b80f71df1b7b41dc52c9e1c2440cd6
gezichenshan <276489620@qq.com>
2018-01-05 18:32:49 +0800

    Update formGenerator.vue

--- src/formGenerator.vue
+++ src/formGenerator.vue
@@ -14,7 +14,8 @@ div.vue-form-generator(v-if='schema != null')
 						button(v-for='btn in field.buttons', @click='buttonClickHandler(btn, field, $event)', :class='btn.classes') {{ btn.label }}
 				.hint(v-if='field.hint') {{ fieldHint(field) }}
 				.errors.help-block(v-if='fieldErrors(field).length > 0')
-					span(v-for='(error, index) in fieldErrors(field)', track-by='index') {{ error }}
+					span(v-for='(error, index) in fieldErrors(field)', v-if='!field.unscapeErrors', v-html="error", track-by='index')
+					span(v-for='(error, index) in fieldErrors(field)', v-if='field.unscapeErrors', track-by='index') {{ error }}
 
 	template(v-for='group in groups')
 		fieldset(:is='tag', :class='getFieldRowClasses(group)')
