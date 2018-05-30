3479df6eba85e61d412fbbb466c272623f5d3d3b
nothingismagick <35242872+nothingismagick@users.noreply.github.com>
2018-05-30 13:51:01 +0200

    add eslint "busting" import

--- quasar-test/src/plugins/form-generator.js
+++ quasar-test/src/plugins/form-generator.js
@@ -1,10 +1,6 @@
-// import something here
+import VueFormGenerator from 'vue-form-generator'
 
-import formGenerator from 'vue-form-generator'
-
-
-
-// leave the export, even if you don't use it
 export default ({ app, router, Vue }) => {
-  // something to do
+  Vue.use(VueFormGenerator)
+  // Vue.prototype.$vueFormGenerator = VueFormGenerator
 }
