2c53afe33bb269936b41c923c408267fd1febd99
shalev.tomer@gmail.com <azaqaqa>
2018-05-31 12:11:41 +0300

    Add placeholder for first Quasar component

---
+++ src/fields/frameworks/quassar/fieldCheckbox.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    input(:id="getFieldID(schema)", type="checkbox", v-model="value", :autocomplete="schema.autocomplete", :disabled="disabled", :name="schema.inputName", :class="schema.fieldClasses")
+</template>
+
+<script>
+    // TODO: change this component to be Quasar component q-input
+	import abstractField from "../../abstractField";
+	
+	export default {
+		mixins: [ abstractField ]
+	};
+</script>
+
+<style lang="sass">
+	.vue-form-generator .field-checkbox input {
+		margin-left: 12px;
+	}
+</style>
