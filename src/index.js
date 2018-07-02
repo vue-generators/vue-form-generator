c87e93226fdadf48d5fe1c5a9003139b1d2cda6f
Lionel Bijaoui <lionel.bijaoui@aviitam.com>
2018-07-02 18:37:11 +0200

    Alternative solution to expose embeded fields

--- src/index.js
+++ src/index.js
@@ -6,10 +6,31 @@ const install = Vue => {
 	Vue.component("VueFormGenerator", module.exports.component);
 };
 
+const forEach = require("lodash").forEach;
+
+let fieldComponents = {};
+
+let coreFields = require.context("./fields/core", false, /^\.\/field([\w-_]+)\.vue$/);
+
+forEach(coreFields.keys(), key => {
+	let compName = key.replace(/^\.\//, "").replace(/\.vue/, "");
+	fieldComponents[compName] = coreFields(key).default;
+});
+
+if (process.env.FULL_BUNDLE) {
+	let Fields = require.context("./fields/optional", false, /^\.\/field([\w-_]+)\.vue$/);
+
+	forEach(Fields.keys(), key => {
+		let compName = key.replace(/^\.\//, "").replace(/\.vue/, "");
+		fieldComponents[compName] = Fields(key).default;
+	});
+}
+
 module.exports = {
 	component,
 	schema,
 	validators,
 	abstractField,
+	fieldComponents,
 	install
 };
