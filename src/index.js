320a4b522d1f402de1eb71246cd2fce2cfb07d5a
Lionel Bijaoui <lionel.bijaoui@aviitam.com>
2018-07-03 10:05:45 +0200

    Code refactoring and fix codacity issues

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 const component = require("./formGenerator.vue").default;
 const schema = require("./utils/schema.js");
 const validators = require("./utils/validators.js").default;
+const fieldComponents = require("./utils/fieldsLoader").default;
 const abstractField = require("./fields/abstractField").default;
-const install = Vue => {
+const install = (Vue) => {
 	Vue.component("VueFormGenerator", module.exports.component);
 };
-
-const forEach = require("lodash").forEach;
-
-let fieldComponents = {};
-
-let coreFields = require.context("./fields/core", false, /^\.\/field([\w-_]+)\.vue$/);
-
-forEach(coreFields.keys(), key => {
-	let compName = key.replace(/^\.\//, "").replace(/\.vue/, "");
-	fieldComponents[compName] = coreFields(key).default;
-});
-
-if (process.env.FULL_BUNDLE) {
-	let Fields = require.context("./fields/optional", false, /^\.\/field([\w-_]+)\.vue$/);
-
-	forEach(Fields.keys(), key => {
-		let compName = key.replace(/^\.\//, "").replace(/\.vue/, "");
-		fieldComponents[compName] = Fields(key).default;
-	});
-}
 
 module.exports = {
 	component,
