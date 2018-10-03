90090961369016fb5808f0b779e7542c24255528
Stuart Clark <stu@rtclark.net>
2018-10-03 10:28:20 +1000

    Added required attribute to textArea.

--- src/fields/core/fieldTextArea.vue
+++ src/fields/core/fieldTextArea.vue
@@ -8,6 +8,7 @@
 		:minlength="schema.min",
 		:placeholder="schema.placeholder",
 		:readonly="schema.readonly",
+		:required="schema.required",
 		:rows="schema.rows || 2",
 		:name="schema.inputName",
 		v-attributes="'input'")
