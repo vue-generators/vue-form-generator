c5d5d77075804cfd0fbf2f3053dcc1010ea9f656
Lionel Bijaoui <lionel.bijaoui@aviitam.com>
2018-10-05 17:15:52 +0200

    Remove dev (preparing for submodule)

--- dev/index.js
+++
@@ -1,6 +0,0 @@
-import Vue from "vue";
-import App from "./app.vue";
-
-new Vue({
-	render: (h) => h(App)
-}).$mount("#app");
--- dev/mixins/utils.js
+++
@@ -1,7 +0,0 @@
-export default {
-	computed: {
-		prettyModel() {
-			return JSON.stringify(this.model, null, 4);
-		}
-	}
-};
