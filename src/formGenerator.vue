b027a11e5f1214f46e0f2e8477b0f001ee976b09
Titas Gailius <titas.gailius@gmail.com>
2019-05-13 13:57:05 +0300

    Update formGenerator.vue

--- src/formGenerator.vue
+++ src/formGenerator.vue
@@ -1,14 +1,16 @@
 <template lang="pug">
 div.vue-form-generator(v-if='schema != null')
-	fieldset(v-if="schema.fields", :is='tag')
-		template(v-for='field in fields')
-			form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options", @validated="onFieldValidated", @model-updated="onModelUpdated")
-
-	template(v-for='group in groups')
-		fieldset(:is='tag', :class='getFieldRowClasses(group)')
-			legend(v-if='group.legend') {{ group.legend }}
-			template(v-for='field in group.fields')
+	slot(name="fields" v-bind="{ fields, errors, options, model }")
+		fieldset(v-if="schema.fields", :is='tag')
+			template(v-for='field in fields')
 				form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options", @validated="onFieldValidated", @model-updated="onModelUpdated")
+
+	slot(name="groups" v-bind="{ groups, errors, options, model }")
+		template(v-for='group in groups')
+			fieldset(:is='tag', :class='getFieldRowClasses(group)')
+				legend(v-if='group.legend') {{ group.legend }}
+					template(v-for='field in group.fields')
+						form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options", @validated="onFieldValidated", @model-updated="onModelUpdated")
 </template>
 
 <script>
