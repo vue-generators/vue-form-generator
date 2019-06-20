c7448b3c79b1814ea73b94ca1638a86cba3573e4
Cyprian Muchoki <cyprian.muchoki@booking.com>
2019-06-20 12:03:00 +0200

    Fix fieldComponents exposure
    Exposing fieldComponents was broken because we were exporting named
    exports in the fieldsLoader.js while we were importing as though
    it was exported as a default export.
    
    In export (utils/fieldsLoader.js):
    
    ```
    module.exports = fieldComponents;
    ```
    
    In import time inside (index.js)
    
    ```
    const fieldComponents = require("./utils/fieldsLoader").default;
    ```
    
    This meant that
    
    ```
    import { fieldComponents } from 'vue-form-generator'
    ```
    would always be undefined, which isn't very nice because for some
    usecases, you might actually want to have access to the components.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const component = require("./formGenerator.vue").default;
 const schema = require("./utils/schema.js");
 const validators = require("./utils/validators.js").default;
-const fieldComponents = require("./utils/fieldsLoader").default;
+const fieldComponents = require("./utils/fieldsLoader");
 const abstractField = require("./fields/abstractField").default;
 const install = (Vue, options) => {
 	Vue.component("VueFormGenerator", module.exports.component);
